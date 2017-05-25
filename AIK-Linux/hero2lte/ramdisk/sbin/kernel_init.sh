406b055e3ff2ab0f635da88b273f584ed77e7b7b
lyapota <lyapic@ukr.net>
2017-05-26 00:56:16 +0300

    clean Synapse support

--- AIK-Linux/hero2lte/ramdisk/sbin/kernel_init.sh
+++ AIK-Linux/hero2lte/ramdisk/sbin/kernel_init.sh
@@ -1,91 +1,86 @@
 #!/system/bin/sh
 
-MODE=$1
-
 log_print() {
-  echo "($MODE) $1" >> /dev/.launch_kinit.log
+  echo "$1" >> /dev/.kinit.log
 }
 
-/sbin/resetprop -v -n ro.boot.warranty_bit 0
-/sbin/resetprop -v -n ro.warranty_bit 0
-
-if [ -f "/dev/.kinit" ]; then
+if [ -f "/dev/.kinit.log" ]; then
   log_print "next start"
   exit;
 fi
 
 log_print "start"
 
-run() {
-    echo "$MODE" >> /dev/.kinit
+/sbin/resetprop -v -n ro.boot.warranty_bit 0
+/sbin/resetprop -v -n ro.warranty_bit 0
 
-    # Mount root as RW to apply tweaks and settings
+# Synapse permissions
+chmod 666 /sys/module/workqueue/parameters/power_efficient
+
+# Mount root as RW to apply tweaks and settings
+ROOTFS_RO=$(busybox mount | grep 'rootfs' | grep -c 'ro,')
+if [ "$ROOTFS_RO" -eq "1" ]; then
     busybox mount -o remount,rw /
-
-    # Synapse
-    chmod 666 /sys/module/workqueue/parameters/power_efficient
+fi
+if [ "$(busybox mount | grep 'rootfs' | grep -c 'ro,')" -eq "1" ]; then
+    su -c "busybox mount -o remount,rw /"
+fi
+if [ "$(busybox mount | grep 'rootfs' | grep -c 'ro,')" -eq "1" ]; then
+    log_print "can't mount rootfs"
+else
     chmod -R 755 /res/*
     ln -fs /res/synapse/uci /sbin/uci
     /sbin/uci
 
-    # default kernel params
-    /sbin/kernel_params.sh
+    # Mount root as RO
+    if [ "$ROOTFS_RO" -eq "1" ]; then
+        busybox mount -o remount,ro /
+        if [ "$(busybox mount | grep 'rootfs' | grep -c 'rw,')" -eq "1" ]; then
+            su -c "busybox mount -o remount,ro /"
+        fi
+    fi
+fi
 
-    busybox mount -o remount,ro /
+# default kernel params
+/sbin/kernel_params.sh
 
-    # Mount as RW to apply tweaks and settings
-    busybox mount -o remount,rw /system
-    busybox mount -o remount,rw /data
+# Make internal storage directory.
+if [ ! -d /data/apollo ]; then
+    DATA_RO=$(busybox mount | grep ' /data ' | grep -c 'ro,')
+    if [ "$DATA_RO" -eq "1" ]; then
+        busybox mount -o remount,rw /data
+    fi
+    mkdir /data/apollo
+    if [ "$DATA_RO" -eq "1" ]; then
+        busybox mount -o remount,ro /data
+    fi
+fi;
 
-    # Make internal storage directory.
-    if [ ! -d /data/apollo ]; then
-	mkdir /data/apollo
+# init.d support
+if [ ! -e /system/etc/init.d ]; then
+    SYSTEM_RO=$(busybox mount | grep ' /system ' | grep -c 'ro,')
+    if [ "$SYSTEM_RO" -eq "1" ]; then
+        busybox mount -o remount,rw /system
+    fi
+    mkdir /system/etc/init.d
+    chown -R root.root /system/etc/init.d
+    chmod -R 755 /system/etc/init.d
+    if [ "$SYSTEM_RO" -eq "1" ]; then
+        busybox mount -o remount,ro /system
+    fi
+fi
+
+# start init.d
+for FILE in /system/etc/init.d/*; do
+
+    su -c ${FILE}
+    resSU=$?
+    if [ $resSU == 0 ]; then
+        log_print "SU run ${FILE}";
+    else
+        /system/bin/sh ${FILE}
+        log_print "SH run ${FILE}";
     fi;
 
-    # init.d support
-    if [ ! -e /system/etc/init.d ]; then
-       mkdir /system/etc/init.d
-       chown -R root.root /system/etc/init.d
-       chmod -R 755 /system/etc/init.d
-    fi
+done;
 
-    # start init.d
-    for FILE in /system/etc/init.d/*; do
-
-        su -c ${FILE}
-        resSU=$?
-        if [ $resSU == 0 ]; then
-            echo "SU run ${FILE}" >> /dev/.kinit;
-        else
-            /system/bin/sh ${FILE}
-            echo "SH run ${FILE}" >> /dev/.kinit;
-        fi;
-
-    done;
-}
-
-case $1 in
-  su )
-    if [ `cat /proc/mounts | grep /magisk >/dev/null 2>&1; echo $?` -ne 0 ]; then
-      log_print "!init"
-      exit;
-    fi
-    log_print "init"
-    run
-    ;;
-
-  supersu )
-    if [ `cat /proc/mounts | grep /su >/dev/null 2>&1; echo $?` -ne 0 ]; then
-      log_print "!init"
-      exit;
-    fi
-    log_print "init"
-    run
-    ;;
-
-  service )
-    log_print "late_start"
-    run
-    ;;
-
-esac
